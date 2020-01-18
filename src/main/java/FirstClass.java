1c9686fda97aab1c69ba0eaecd0637e91e3e7838
Przemek Przybylkiewicz <typrzemek@gmail.com>
2020-01-18 21:27:20 +0100

    Zadanie 2.10.

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,16 +1,23 @@
 public class FirstClass {
     public static void main(String[] args) {
-        String helloFirst ="Hello from FirstClass!!";
-        System.out.println(helloFirst);
-        Notebook notebook = new Notebook("600g", 1000);
-        Notebook heavyNetbook = new Notebook("2000g", 1500);
-        Notebook oldNetebook = new Notebook("1600", 500);
-        System.out.println(notebook.weight +" "+ notebook.price);
+        String hello ="Welcome in the Cyberstore traveler";
+        System.out.println(hello);
+        Notebook notebook = new Notebook(600, 1000, 2020);
+        Notebook heavyNetbook = new Notebook(2000, 1500, 2010);
+        Notebook oldNetebook = new Notebook(1600, 500 , 2000);
+        System.out.println(notebook.weight+"g" +" "+ notebook.price+"zl" +" "+notebook.year+"r.");
         notebook.checkprice();
-        System.out.println(heavyNetbook.weight +" "+ heavyNetbook.price);
+        notebook.checkWeight();
+        notebook.checkBestComp();
+        System.out.println(heavyNetbook.weight +"g" +" "+ heavyNetbook.price+"zl"+" "+heavyNetbook.year+"r.");
         heavyNetbook.checkprice();
-        System.out.println(oldNetebook.weight + " " + oldNetebook.price);
+        heavyNetbook.checkWeight();
+        heavyNetbook.checkBestComp();
+        System.out.println(oldNetebook.weight+"g" + " " + oldNetebook.price+"zl" +" "+oldNetebook.year+"r.");
         oldNetebook.checkprice();
+        oldNetebook.checkWeight();
+        oldNetebook.checkBestComp();
+
 
     }
 }
--- src/main/java/Notebook.java
+++ src/main/java/Notebook.java
@@ -1,16 +1,19 @@
 public class Notebook {
-    String weight;
+    int weight;
     int price;
+    int year;
 
-    public Notebook(String weight, int price) {
+
+    public Notebook(int weight, int price , int year) {
         this.weight = weight;
         this.price = price;
-
+        this.year = year;
 
     }
 
+
     public void checkprice() {
-        if (this.price < 600) {
+        if (this.price <= 600) {
             System.out.println("this is a cheap");
         }else if((this.price >= 600) && (this.price <= 1000)) {
             System.out.println("The price is good");
@@ -18,4 +21,32 @@ public class Notebook {
             System.out.println("This notebook is expensive");
         }
     }
+    public void checkWeight(){
+        if (this.weight <= 600){
+            System.out.println("this computer is lightweight");
+        }else if((this.weight>=600) && (this.weight<=1600)){
+            System.out.println("this computer is heavy");
+        }else if (this.weight>=2000){
+            System.out.println("this computer is super heavy");
+        }
+    }
+    public void checkyear(){
+        if(this.year>=2020){
+            System.out.println("good year man");
+        }else if ((this.year<=2000)&&(this.year>=2010)){
+            System.out.println("it can still be useful");
+        }else{
+            System.out.println("don't buy this crap");
+        }
+    }
+    public void checkBestComp() {
+        if ((this.price <= 1000)&&(this.weight<=1000)&&(this.year >= 2015)) {
+            System.out.println("that's it, buy it");
+        }else if((this.price <= 2000)&&(this.weight<=2000)&&(this.year>=2010)){
+            System.out.println("it may be useful");
+        }else{
+            System.out.println("don't buy this crap");
+        }
+    }
 }
+
