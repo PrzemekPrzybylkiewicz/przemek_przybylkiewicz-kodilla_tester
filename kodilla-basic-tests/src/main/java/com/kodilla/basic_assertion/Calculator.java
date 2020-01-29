777d37ba62c19eaa2e73f77cc6aa3e66f09fce40
Przemek Przybylkiewicz <typrzemek@gmail.com>
2020-01-29 15:54:08 +0100

    calculator

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
@@ -0,0 +1,24 @@
+package com.kodilla.basic_assertion;
+
+import static java.lang.Math.pow;
+
+public class Calculator {
+    int x;
+    int y;
+
+
+    public int addNum(int x,int y){
+       return  x+y;
+    }
+    public int subtruckNum( int x,int y){
+        return x-y;
+    }
+    public double powNum(double x,double y){
+        double pow = pow(x ,y);
+        return pow;
+    }
+
+
+
+
+}
