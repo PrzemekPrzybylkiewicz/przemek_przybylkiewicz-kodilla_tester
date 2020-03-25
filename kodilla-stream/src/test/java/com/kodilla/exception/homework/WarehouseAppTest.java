0b1fd509e1b28320fc32ba85cbb2dd77f9d6e8fe
Przemek Przybylkiewicz <typrzemek@gmail.com>
2020-03-25 15:28:55 +0100

    25.03.- pamiętniki z kwarantanny

---
+++ kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseAppTest.java
@@ -0,0 +1,22 @@
+package com.kodilla.exception.homework;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class WarehouseAppTest {
+
+    @Test
+    public void testIsExeptionIsCorrect() throws OrderDoesntExistException {
+        //given
+        Warehouse warehouse = new Warehouse();
+        //when
+        Order testExeption = warehouse.getOrder("12");
+        //then
+        assertTrue(testExeption);
+    }
+
+}
